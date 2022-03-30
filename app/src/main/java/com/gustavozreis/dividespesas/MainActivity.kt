343efabdb59981bfc7ace0e9117f9ece41b198de
gustavozreis <gzreis@gmail.com>
2022-03-30 18:33:02 -0300

    Início do projeto.

---
+++ app/src/main/java/com/gustavozreis/dividespesas/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.gustavozreis.dividespesas
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.gustavozreis.dividespesas.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
