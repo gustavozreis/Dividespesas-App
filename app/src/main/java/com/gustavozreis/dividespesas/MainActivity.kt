53e750e35f4750ca356ac25f24c960631436ff6e
gustavozreis <gzreis@gmail.com>
2022-04-07 13:40:49 -0300

    Inicia Implementação Conceito MVVM/CleanArchitecture.
    Divisão dos pacotes do projeto, criação de repositories, datasource, usecases e models.

--- app/src/main/java/com/gustavozreis/dividespesas/MainActivity.kt
+++ app/src/main/java/com/gustavozreis/dividespesas/MainActivity.kt
@@ -2,11 +2,31 @@ package com.gustavozreis.dividespesas
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.widget.Button
+import android.widget.TextView
+import androidx.lifecycle.lifecycleScope
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.ktx.Firebase
 import com.gustavozreis.dividespesas.R
+import com.gustavozreis.dividespesas.databinding.ActivityMainBinding
+import com.gustavozreis.dividespesas.domain.models.Spend
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        var btnTeste: Button = binding.button2
+        var textTeste: TextView = binding.tvTest
+
+
+
+
     }
 }
--- app/src/main/java/com/gustavozreis/dividespesas/bases/BaseActivity.kt
+++ app/src/main/java/com/gustavozreis/dividespesas/bases/BaseActivity.kt
@@ -2,6 +2,8 @@ package com.gustavozreis.dividespesas.bases
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.ktx.Firebase
 
 class BaseActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
